001837d3186a7a4e2c7a2ed742cca848151afc73
rohitrainbow <rohitrainbow@yahoo.co.in>
2018-05-28 18:13:53 +0530

    commit2

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.android.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void submitOrder(View view)
+    {
+        display(2);
+        priceDisplay(2*5);
+    }
+
+    private void display(int number){
+        TextView quantityTextView=(TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(""+number);
+    }
+
+    private void priceDisplay(int number)
+        {
+            TextView priceTextView=(TextView) findViewById(R.id.price_text_view);
+            priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        }
 }
