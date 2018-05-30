21080be9218f0ee9e91e9f25b43d7e0b9e998b25
rohitrainbow <rohitrainbow@yahoo.co.in>
2018-05-30 11:10:29 +0530

    Final Commit

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,26 +9,52 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Integer numberOfCoffees = 0;
+    String priceMessage="Free";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView priceTextView=(TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(numberOfCoffees*5));
     }
 
-    public void submitOrder(View view)
-    {
-        display(2);
-        priceDisplay(2*5);
-    }
-
-    private void display(int number){
-        TextView quantityTextView=(TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(""+number);
-    }
-
-    private void priceDisplay(int number)
+    public void submitOrder(View view) {
+        priceMessage="Free";
+        if(numberOfCoffees<=0)
         {
-            TextView priceTextView=(TextView) findViewById(R.id.price_text_view);
-            priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+            priceMessage="Please enter correct quantity";
         }
+        else
+        {
+            priceMessage="Total: "+NumberFormat.getCurrencyInstance().format(numberOfCoffees*5)+"\nThank You!";
+        }
+        displayMessage(priceMessage);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    public void increment(View view) {
+        display(++numberOfCoffees);
+    }
+
+    public void decrement(View view) {
+        display(--numberOfCoffees);
+    }
+
+    private void priceDisplay() {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(priceMessage);
+    }
+
+    private void displayMessage(String message)
+    {
+        TextView priceTextView=(TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
 }
